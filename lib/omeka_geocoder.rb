a6b95b9c27c4100952a15643fdaa2d79e61c4d96
Wayne Graham <wayne.graham@virginia.edu>
2014-12-12 20:16:45 -0500

    Initial layout

--- lib/omeka_geocoder.rb
+++ lib/omeka_geocoder.rb
@@ -1,5 +1,85 @@
 require "omeka_geocoder/version"
 
+require 'csv'
+require 'geocoder'
+require 'optparse'
+require 'ostruct'
+
 module OmekaGeocoder
-  # Your code goes here...
+
+  class OptionsParser
+    def self.parse(args)
+      options = OpenStruct.new
+      options.verbose = false
+
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = "Usage: omeka_geocoder [options] path/to/addresses.csv"
+
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        opts.on("-v", "--[no]-verbose", "Run verbosely") do |v|
+          options.verbose = v
+        end
+
+        opts.separator ""
+
+        opts.separator "Common options:"
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+
+        opts.on_tail("--version", "Show version") do
+          puts VERSION
+          exit
+        end
+      end
+
+      opt_parser.parse!(args)
+      options
+    end
+  end
+
+  class CLI
+
+    attr_accessor :file
+
+    def initialize(*args)
+      if(File.file?(args[0][0]))
+        @file = args[0][0]
+        @csv = CSV.read(@file, { headers: true })
+      end
+    end
+
+    def geocode
+
+      @csv.each do |line|
+        address = make_address(line)
+        result = Geocoder.search(address).first
+
+        projected = degrees_to_meters(
+          result.longitude,
+          result.latitude
+        )
+        puts projected['longitude']
+      end
+    end
+
+    def make_address(line)
+      "#{line['Address']}, #{line['City']}, #{line['State']}"
+    end
+
+    def degrees_to_meters(lon, lat)
+      half_circumference = 20037508.34
+      x = lon * half_circumference / 180
+      y = Math.log(Math.tan((90 + lat) * Math::PI / 360)) / (Math::PI / 180)
+      y = y * half_circumference / 180
+      return {"latitude" => x, "longitude" => y}
+    end
+
+    def run
+      geocode
+    end
+  end
 end
--- omeka_geocoder.gemspec
+++ omeka_geocoder.gemspec
@@ -8,16 +8,18 @@ Gem::Specification.new do |spec|
   spec.version       = OmekaGeocoder::VERSION
   spec.authors       = ["Wayne Graham"]
   spec.email         = ["wayne.graham@virginia.edu"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
-  spec.homepage      = ""
+  spec.summary       = %q{A Simple Geocoder for Omeka's NeatlineFeatures plugin}
+  spec.homepage      = "https://github.com/waynegraham/omeka_geocoder"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0")
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_runtime_dependency "geocoder", "~> 1.2"
+  spec.add_runtime_dependency "mechanize", "~> 2.7"
+
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rspec"
 end
